c4724865c88ac060d7b1b8d06d6b3285d73ea078
Salwa-Mansour <alqadasisal@gmail.com>
2021-01-03 22:16:01 +0300

    h

---
+++ imgupload/db.php
@@ -0,0 +1,20 @@
+<?php
+    function createdb(){
+
+ $con=mysqli_connect('localhost','root','');
+ $sql="CREATE DATABASE IF NOT EXISTS imgdb";
+
+ if( mysqli_query($con,$sql)){
+     $sql='CREATE TABLE IF NOT EXISTS imgs(
+         img BLOB ,
+         bio VARCHAR(300)
+     )';
+    $con=mysqli_connect('localhost','root','','imgdb');
+
+ }else{echo"connect field".mysqli_connect_error();}
+   if(mysqli_query($con,$sql)){
+      return $con;
+   }else{echo"databs create error".mysqli_error($con);}
+}
+   
+?>
